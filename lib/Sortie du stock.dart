46bb9facf15597d138880c2e94d5d95d59759f48
Chamalo <aminestar1@hotmail.com>
2021-03-08 23:44:26 +0100

    ..

--- lib/Sortie du stock.dart
+++ lib/Sortie du stock.dart
@@ -18,58 +18,80 @@ class _ExampleState extends State<Example> {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          backgroundColor: Colors.blueGrey,
+          backgroundColor: Color(0xff62959c),
           title: Text('Sortie du stock'),
           centerTitle: true,
         ),
         body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Container(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  DropdownButton(
-                    hint: Text('Selectionner depot'),
+              margin: EdgeInsets.only(left: 5, top: 10, right: 5, bottom: 10),
+              width: 350.0,
+              decoration: ShapeDecoration(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30),
+                  side: BorderSide(width: 1.0, style: BorderStyle.solid),
+                ),
+              ),
+              child: DropdownButtonHideUnderline(
+                child: ButtonTheme(
+                  alignedDropdown: true,
+                  child: DropdownButton(
+                    hint: Text('Sélectionner un entrepôt'),
+                    icon: Icon(Icons.arrow_drop_down),
+                    iconSize: 42,
                     value: _selectedLocation,
+                    items: _locations.map((location) {
+                      return DropdownMenuItem(
+                        child: new Text(location,
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold)),
+                        value: location,
+                      );
+                    }).toList(),
                     onChanged: (newValue) {
                       setState(() {
                         _selectedLocation = newValue;
                       });
                     },
-                    items: _locations.map((location) {
-                      return DropdownMenuItem(
-                        child: new Text(location),
-                        value: location,
-                      );
-                    }).toList(),
-                  ),
-                ],
-              ),
-            ),
-            Image.asset(
-              'images/scan.png',
-              cacheWidth: 270,
-              cacheHeight: 200,
-            ),
-            Container(
-              child: FlatButton(
-                child: Text('Scanner code a barre'),
-                color: Colors.amber,
-                textColor: Colors.white,
-                minWidth: 300,
-                height: 50,
-                onPressed: () {},
-              ),
-            ),
-            Row(
-              children: [
-                Container(
-                  child: Text(
-                    'Produit ajoutées',
-                    style: TextStyle(fontSize: 20),
+                    style: Theme.of(context).textTheme.title,
                   ),
                 ),
-              ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 5.0, bottom: 15),
+              child: Center(
+                child: Image.asset(
+                  'images/scan.png',
+                  cacheWidth: 300,
+                  cacheHeight: 100,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Center(
+                child: Container(
+                  child: FlatButton(
+                    child: Text('Scanner code a barre'),
+                    color: Color(0xffec524b),
+                    textColor: Colors.white,
+                    minWidth: 350,
+                    height: 50,
+                    onPressed: () {},
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              child: Text(
+                'Produit ajoutées',
+                style: TextStyle(fontSize: 20),
+              ),
             ),
           ],
         ),
--- lib/TrasfertDuStock.dart
+++ lib/TrasfertDuStock.dart
@@ -5,15 +5,14 @@ void main() {
   runApp(Example());
 }
 
-
 class Example extends StatefulWidget {
   @override
   State<StatefulWidget> createState() => _ExampleState();
 }
 
 class _ExampleState extends State<Example> {
-  List<String> _locations = ['Msaken', 'monastir', 'Sousse', 'beja'];
-  List<String> _locations1 = ['Msaken', 'monastir', 'Sousse', 'beja'];
+  List<String> _locations = ['Msaken', 'Monastir', 'Sousse', 'Beja'];
+  List<String> _locations1 = ['Msaken', 'Monastir', 'Sousse', 'Beja'];
   String _selectedLocation;
   String _selectedLocation1;
 
@@ -30,35 +29,43 @@ class _ExampleState extends State<Example> {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Row(
-            
-              children: [ 
+              children: [
                 Padding(
-                  padding: const EdgeInsets.only(top:8.0,left:20),
+                  padding: const EdgeInsets.only(top: 8.0, left: 15),
                   child: Text("Choix d'entrepôt (origine)"),
-                ), SizedBox(width: 90),
+                ),
+                SizedBox(width: 90),
                 Padding(
-                  padding: const EdgeInsets.only(top:8.0),
+                  padding: const EdgeInsets.only(top: 8.0),
                   child: Transform(
                       alignment: Alignment.center,
                       transform: Matrix4.rotationY(math.pi),
-                      child: Icon(Icons.reply,color: Colors.red,)),
+                      child: Icon(
+                        Icons.reply,
+                        color: Colors.red,
+                      )),
                 )
               ],
             ),
             Container(
-              margin: EdgeInsets.only(left: 20),
-              width: 350.0,  decoration: ShapeDecoration(
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30),
-          side: BorderSide(width: 1.0, style: BorderStyle.solid),),),
+              margin: EdgeInsets.only(left: 10),
+              width: 350.0,
+              decoration: ShapeDecoration(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30),
+                  side: BorderSide(width: 1.0, style: BorderStyle.solid),
+                ),
+              ),
               child: DropdownButtonHideUnderline(
                 child: ButtonTheme(
-                  alignedDropdown: true, 
-                  child: DropdownButton(icon: Icon(Icons.arrow_drop_down),
-                   iconSize: 42,
-                  
+                  alignedDropdown: true,
+                  child: DropdownButton(
+                    hint: Text('Sélectionner un entrepôt'),
+                    icon: Icon(Icons.arrow_drop_down),
+                    iconSize: 42,
                     value: _selectedLocation,
                     items: _locations.map((location) {
-                      return DropdownMenuItem( 
+                      return DropdownMenuItem(
                         child: new Text(location,
                             style: TextStyle(
                                 color: Colors.black,
@@ -78,27 +85,35 @@ class _ExampleState extends State<Example> {
               ),
             ),
             Row(
-             
               children: [
                 Padding(
-                  padding: const EdgeInsets.only(top:8.0,left:20),
+                  padding: const EdgeInsets.only(top: 8.0, left: 20),
                   child: Text("Choix d'entrepôt (destination)"),
-                ),SizedBox(width: 58),Icon(Icons.reply,color: Colors.green,),
-                
+                ),
+                SizedBox(width: 58),
+                Icon(
+                  Icons.reply,
+                  color: Colors.green,
+                ),
               ],
             ),
             Container(
-              margin: EdgeInsets.only(left: 20),
-              width: 350.0,  decoration: ShapeDecoration(
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30),
-          side: BorderSide(width: 1.0, style: BorderStyle.solid),),),
+              margin: EdgeInsets.only(left: 10),
+              width: 350.0,
+              decoration: ShapeDecoration(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30),
+                  side: BorderSide(width: 1.0, style: BorderStyle.solid),
+                ),
+              ),
               child: DropdownButtonHideUnderline(
                 child: ButtonTheme(
                   alignedDropdown: true,
                   child: DropdownButton(
-      icon: Icon(Icons.arrow_drop_down),
-      iconSize: 42,
-      underline: SizedBox(),
+                    hint: Text('Sélectionner un entrepôt'),
+                    icon: Icon(Icons.arrow_drop_down),
+                    iconSize: 42,
+                    underline: SizedBox(),
                     value: _selectedLocation1,
                     items: _locations.map((location1) {
                       return DropdownMenuItem(
@@ -121,9 +136,9 @@ class _ExampleState extends State<Example> {
               ),
             ),
             Padding(
-              padding: const EdgeInsets.only(top:20.0),
+              padding: const EdgeInsets.only(top: 20.0),
               child: Center(
-                child: Image.asset( 
+                child: Image.asset(
                   'images/scan.png',
                   cacheWidth: 300,
                   cacheHeight: 100,
@@ -136,7 +151,7 @@ class _ExampleState extends State<Example> {
                 child: Container(
                   child: FlatButton(
                     child: Text('Scanner code a barre'),
-                     color: Color(0xffec524b),
+                    color: Color(0xffec524b),
                     textColor: Colors.white,
                     minWidth: 350,
                     height: 50,
