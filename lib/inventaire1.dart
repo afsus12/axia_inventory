e6455f26c959ed4c5622e0b94b7622fb713146d9
Chamalo <aminestar1@hotmail.com>
2021-03-10 00:38:57 +0100

    .

--- lib/inventaire1.dart
+++ lib/inventaire1.dart
@@ -14,7 +14,117 @@ class _inventaireState extends State<inventaire> {
         backgroundColor: Color(0xff62959c),
         title: Text('Inventaire'),
         centerTitle: true,
+        automaticallyImplyLeading: false,
       ),
-          );
+      body: Column(
+        children: [
+          Row(
+            children: [
+              Row(
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(left: 75),
+                    width: 200,
+                    child: TextFormField(
+                      cursorColor: Theme.of(context).cursorColor,
+                      initialValue: '',
+                      decoration: InputDecoration(
+                        labelText: 'N°inventaire :',
+                        labelStyle: TextStyle(
+                          color: Color(0xFF6200EE),
+                        ),
+                        enabledBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Color(0xFF6200EE)),
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              Container(
+                margin: EdgeInsets.only(left: 75),
+                width: 200,
+                child: TextFormField(
+                  cursorColor: Theme.of(context).cursorColor,
+                  initialValue: '',
+                  decoration: InputDecoration(
+                    labelText: 'Entrepôt',
+                    labelStyle: TextStyle(
+                      color: Color(0xFF6200EE),
+                    ),
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Color(0xFF6200EE)),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+          Row(
+            children: [
+              Container(
+                margin: EdgeInsets.only(left: 75),
+                width: 200,
+                child: TextFormField(
+                  cursorColor: Theme.of(context).cursorColor,
+                  initialValue: '',
+                  decoration: InputDecoration(
+                    labelText: 'Réferance Inv :',
+                    labelStyle: TextStyle(
+                      color: Color(0xFF6200EE),
+                    ),
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Color(0xFF6200EE)),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+          Row(
+            children: [
+              Container(
+                margin: EdgeInsets.only(left: 75),
+                width: 200,
+                child: TextFormField(
+                  cursorColor: Theme.of(context).cursorColor,
+                  initialValue: '',
+                  decoration: InputDecoration(
+                    labelText: 'Collaborateur :',
+                    labelStyle: TextStyle(
+                      color: Color(0xFF6200EE),
+                    ),
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Color(0xFF6200EE)),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 20),
+            child: Center(
+              child: Container(
+                child: FlatButton(
+                  child: Text('Scanner code a barre'),
+                  color: Color(0xffec524b),
+                  textColor: Colors.white,
+                  minWidth: 210,
+                  height: 50,
+                  onPressed: () {
+                    setState(() {});
+                  },
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
