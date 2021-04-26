3eac39b7b3c32ab60caed8980ae3136707414d1e
afsa yassine <afsayassine@gmail.com>
2021-04-26 02:43:15 +0100

    vddsf

--- lib/gestion3.dart
+++ lib/gestion3.dart
@@ -26,6 +26,7 @@ class DataFromAPI extends StatefulWidget {
 }
 
 class _DataFromAPIState extends State<DataFromAPI> {
+    static List mainDataList ;
   Future getUserData() async {
     var response = await http.get(
       Uri.parse('https://192.168.1.9:8000/api/users/getusers'),
@@ -34,6 +35,10 @@ class _DataFromAPIState extends State<DataFromAPI> {
     List<User> users = [];
     for (var u in jsonData) {
       User user = User(u["protmUser"], u["deCode"], u["cbcreateur"]);
+      
+    
+     
+    
       users.add(user);
     }
     print(users.length);
@@ -41,6 +46,17 @@ class _DataFromAPIState extends State<DataFromAPI> {
   }
 
   TextEditingController _textController = TextEditingController();
+
+
+  List<String> newDataList = List.from(mainDataList);
+
+  onItemChanged(String value) {
+    setState(() {
+      newDataList = mainDataList
+          .where((string) => string.toLowerCase().contains(value.toLowerCase()))
+          .toList();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -277,7 +293,8 @@ class _DataFromAPIState extends State<DataFromAPI> {
                   controller: _textController,
                   decoration: InputDecoration(
                     hintText: 'Search Here...',
-                  ),
+                    
+                  ), onChanged: onItemChanged,
                 ),
               ),
               RaisedButton(
