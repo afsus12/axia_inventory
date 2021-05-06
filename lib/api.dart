9ad8934a7aab54813fe0c9963722b3ce0e11f12c
Chamalo <aminestar1@hotmail.com>
2021-05-06 03:23:26 +0100

    vv

--- lib/api.dart
+++ lib/api.dart
@@ -3,6 +3,18 @@ import 'dart:convert';
 import 'dart:io';
 import 'package:http/http.dart' as http;
 
+Future<http.Response> role(
+  int admin,
+  String cb,
+) async {
+  var url = 'https://192.168.1.34:8000/api/users/addroles';
+
+  Map data = {
+    'protmAdministrator': admin,
+    'cbCreateur': cb,
+  };
+}
+
 Future<http.Response> loginUser(String user, String pass, String uri) async {
   var url = 'https://$uri/login';
 
